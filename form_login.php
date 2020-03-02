7cb81922e09b4d77251c22ae7d290924e168a630
Oksana465 <61194260+Oksana465@users.noreply.github.com>
2020-03-02 12:20:19 +0300

    Add files via upload

---
+++ form_login.php
@@ -0,0 +1,26 @@
+<?php header("Content-Type: text/html; charset=CP1251");
+
+//Сделать эту форму доступной только авторизованным пользователям:
+//Создать простую форму регистрации (ФИО, телефон, e-mail), проверить корректность ее заполнения
+//Добавить к форме регистрации поле с файлом резюме, организовать его сохранение
+//*Организовать сохранение зарегистрированных пользователей
+
+
+if (isset($_POST['send'])) {
+$surname = $_POST['surname'];
+$name = $_POST['name'];
+$tel = $_POST['tel'];
+$email = $_POST['email'];
+$file = $_FILES['file']['name'];
+
+$f = fopen('file.txt', 'a');
+fwrite ($f, 'Фамилия: ' . $surname . "\n");
+fwrite ($f, 'Имя: ' . $name . "\n");
+fwrite ($f, 'Телефон: ' . $tel . "\n");
+fwrite ($f, 'e-mail: ' . $email . "\n");
+fwrite ($f, 'Резюме: ' . $file . "\n");
+fwrite ($f, ' ' . "\n");
+fclose($f);
+}
+
+
