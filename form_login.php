cc1d550e1fe47fdcd683608b372ba8ec51a81a22
cherep721 <alexnorin721@gmail.com>
2020-03-16 13:14:04 +0300

    check homework

--- form_login.php
+++ form_login.php
@@ -1,12 +1,13 @@
-<?php header("Content-Type: text/html; charset=CP1251");
-
+<?php
 //Сделать эту форму доступной только авторизованным пользователям:
 //Создать простую форму регистрации (ФИО, телефон, e-mail), проверить корректность ее заполнения
 //Добавить к форме регистрации поле с файлом резюме, организовать его сохранение
 //*Организовать сохранение зарегистрированных пользователей
 
+if (!isset($_POST['send'])) {
+    return;
+}
 
-if (isset($_POST['send'])) {
 $surname = $_POST['surname'];
 $name = $_POST['name'];
 $tel = $_POST['tel'];
@@ -15,17 +16,16 @@ $pass = $_POST['pass'];
 $file = $_FILES['file']['name'];
 
 $f = fopen('file.txt', 'a');
-fwrite ($f, 'Фамилия: ' . $surname . "\n");
-fwrite ($f, 'Имя: ' . $name . "\n");
-fwrite ($f, 'Телефон: ' . $tel . "\n");
-fwrite ($f, 'e-mail: ' . $email . "\n");
-fwrite ($f, 'Пароль: ' . $pass . "\n");
-fwrite ($f, 'Резюме: ' . $file . "\n");
-fwrite ($f, '    ' . "\n");
+fwrite($f, 'Фамилия: ' . $surname . "\n");
+fwrite($f, 'Имя: ' . $name . "\n");
+fwrite($f, 'Телефон: ' . $tel . "\n");
+fwrite($f, 'e-mail: ' . $email . "\n");
+fwrite($f, 'Пароль: ' . $pass . "\n");
+fwrite($f, 'Резюме: ' . $file . "\n");
+fwrite($f, '    ' . "\n");
 fclose($f);
 
-setcookie("Cookie", $email . $pass , time()+10800);
+setcookie("Cookie", $email . $pass, time() + 10800);
 header("Location: http://localhost/summ.html");
-}
 
 
--- php1/1.php
+++ php1/1.php
@@ -1,33 +1,36 @@
-<?php header("Content-Type: text/html; charset=CP1251");
+<?php
 //Задание 1 - Управляющие констукции + циклы
 //Написать скрипт, считающий сумму цифр числа $a. Например: есть число 123, сумма цифр 1 + 2 + 3 = 6
 //Проверить корректность данных: что вообще число, что целое и т.п.
 
 echo 'Введите Исходное число: <form action="" method=post>';
 echo '<input type=text name="ab" value=""><br><input type=submit></form>';
-$a = '';
 
-if(isset($_POST['ab']))
-{
-$a=$_POST['ab'];
+if (!isset($_POST['ab'])) {
+    exit();
 }
-else exit();
 
-$pos1 = stripos($a, '.');
-$pos2 = stripos($a, ',');
-if ($pos1 || $pos2) {
-        echo "'{$a}' - не целое число";
-    } else if(is_numeric($a)){
+$a = $_POST['ab'];
+
+if (!is_int($a)) {
+    $type = 'НЕ число';
+    if (is_numeric($a)) {
+        $type = 'НЕ целое число';
+    } elseif ($a === '') {
+        $type = "Пустая строка";
+    }
+    echo "'{$a}' - " . $type;
+    exit;
+}
+
+
 //обнуляем переменную
-$sum=0;
+$sum = 0;
 //приводим число к массиву
 $b = str_split($a);
-//ищем сумму 
-$sum=array_sum($b);
+//ищем сумму
+$sum = array_sum($b);
 
 //выводи результат
 echo "Исходное число: " . $a . "<br>";
 echo "Сумма цифр исходного числа: " . $sum;
-}
-else if ($a=='') { echo "Пустая строка"; exit;} 
-else {echo "'{$a}' - НЕ число";}
